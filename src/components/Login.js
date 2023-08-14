6bee8206305d8dad30cfeb6a2b5e28bd1c443fff
Jan Golder <kontakt.jangolder@gmail.com>
2023-08-14 20:01:58 +0200

    update login component

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,4 +1,5 @@
 import Card from "../UI/Card";
+import Button from "../UI/Button";
 import classes from './Login.module.css';
 
 const Login = ()=>{
@@ -6,7 +7,12 @@ const Login = ()=>{
 return (
     <section className={classes.login}>
         <Card>
-           LOGIN PANEL 
+           <h1>Log in and enjoy fresh products!</h1>
+           <form>
+                <input placeholder="Email address" type="email"/>
+                <input placeholder="Password" type="text"/>
+                <Button>Log in</Button>
+           </form>
         </Card>
     </section>
 )
