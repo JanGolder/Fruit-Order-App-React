4a508c23365a5dcfb00accaf4d8f5407ed872a04
Jan Golder <kontakt.jangolder@gmail.com>
2023-08-16 19:16:32 +0200

    add basic structure of Admin Panel

---
+++ src/components/AdminPanel.js
@@ -0,0 +1,11 @@
+import classes from './AdminPanel.module.css';
+
+const AdminPanel = ()=>{
+    return (
+        <section className={classes['admin-wrap']}>
+            Admin Panel
+        </section>
+    )
+};
+
+export default AdminPanel;
