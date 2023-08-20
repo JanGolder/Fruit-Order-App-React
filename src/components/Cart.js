d667640ffec8de9d007f18e4369c39867c2794a7
Jan Golder <kontakt.jangolder@gmail.com>
2023-08-20 16:17:05 +0200

    add modal handler

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,8 +1,17 @@
 import classes from './Cart.module.css';
+import { useContext } from 'react';
+import AuthContext from '../context/auth-context';
 
 const Cart = ()=>{
+
+const ctx = useContext(AuthContext);
+
+const modalHandler = ()=>{
+    ctx.onModalActive();
+}
+
     return (
-        <div className={classes['cart-wrap']}>
+        <div className={classes['cart-wrap']} onClick={modalHandler}>
             <p className={classes['cart-text']}>Your Cart</p>
             <div className={classes['cart-amount']}>3</div>
         </div>
