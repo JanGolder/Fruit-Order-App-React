a8e842bf742475a58de088b94810e9af3e8740af
Jan Golder <kontakt.jangolder@gmail.com>
2023-08-22 20:59:48 +0200

    update button structure

--- src/UI/Button.js
+++ src/UI/Button.js
@@ -3,7 +3,7 @@ import classes from './Button.module.css'
 const Button = (props)=>{
 
     return (
-        <button className={`${classes.button} ${props.isWhite ? classes.white : ''}`}>{props.children}</button>
+        <button onClick={props.onClose} disabled={props.disabled} className={`${classes.button} ${props.isWhite ? classes.white : ''}`}>{props.children}</button>
     );
 }
 
