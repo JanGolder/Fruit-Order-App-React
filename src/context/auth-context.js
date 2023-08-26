b0c2dfd56d029398615a149b366f971deb8d3d03
Jan Golder <kontakt.jangolder@gmail.com>
2023-08-26 18:42:07 +0200

    add new functions to context

--- src/context/auth-context.js
+++ src/context/auth-context.js
@@ -1,13 +1,20 @@
 import React, { useState, useEffect } from "react";
 
 const AuthContext = React.createContext({
+  productsInCart: [],
   isLoggedIn: false,
+  hasProducts: false,
   onLogout: () => {},
   onLogin: () => {},
+  onModalActive: ()=>{},
+  productsCartUpdate: ()=>{},
 });
 
 export const AuthContextProvider = (props) => {
+  const [productsInCart, setProductsInCart] = useState([]);
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [isModalActive, setIsModalActive] = useState(false);
+  const [hasProducts, setHasProducts] = useState(true);
 
   useEffect(() => {
     const storedUserLogInfo = localStorage.getItem("isLoggedIn");
@@ -16,6 +23,10 @@ export const AuthContextProvider = (props) => {
       setIsLoggedIn(true);
     }
   }, []);
+
+  const modalActiveHandler = ()=>{
+    setIsModalActive(isModalActive=> !isModalActive);
+  }
 
   const logoutHandler = () => {
     localStorage.removeItem("isLoggedIn");
@@ -27,12 +38,22 @@ export const AuthContextProvider = (props) => {
     setIsLoggedIn(true);
   };
 
+const productsCartUpdate = (data)=>{
+  setProductsInCart(prevState=> [...prevState,data]);
+  console.log(productsInCart)
+}
+
   return (
     <AuthContext.Provider
       value={{
+        productsInCart:productsInCart,
+        cartUpdate:productsCartUpdate,
         isLoggedIn: isLoggedIn,
         onLogout: logoutHandler,
         onLogin: loginHandler,
+        isModalActive: isModalActive,
+        onModalActive: modalActiveHandler,
+        hasProducts:hasProducts,
       }}
     >
       {props.children}
