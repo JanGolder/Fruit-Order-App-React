d179cc5976e8ac288f6a70a4e9a58ea5abfdd16f
Jan Golder <kontakt.jangolder@gmail.com>
2023-09-03 20:18:05 +0200

    add scratch of admin panel

--- src/components/AdminPanel.js
+++ src/components/AdminPanel.js
@@ -4,6 +4,8 @@ const AdminPanel = ()=>{
     return (
         <section className={classes['admin-wrap']}>
             Admin Panel
+            {/* add new product - inputs
+                edit / remove products - list of products with filter, action when clicked*/}
         </section>
     )
 };
