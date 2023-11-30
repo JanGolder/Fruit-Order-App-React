02a5a2aaf65d204363d3ab5836cbc1da2e0f0cf9
Jan Golder <kontakt.jangolder@gmail.com>
2023-11-30 19:01:42 +0100

    add dynamic list of products in admin panel

--- src/components/ShortProductList.js
+++ src/components/ShortProductList.js
@@ -1,58 +1,61 @@
 import { useCallback, useState, useEffect } from "react";
-import Card from "../UI/Card";
+import ShortProduct from "./ShortProduct";
+import classes from './ShortProductList.module.css';
 
 
-const ShortProductList =()=>{
+const ShortProductList =(props)=>{
 
-    const [isLoading, setIsLoading] = useState(false);
-    const [products, setProducts] = useState();
-    const [error, setError] = useState(false);
+  const {onRemoveProduct, products} = props
 
-    const fetchProductsHandler = useCallback(async () => {
-        setIsLoading(true);
-        setError(null);
-        try {
-          const response = await fetch(
-            "https://greenfood-3fadf-default-rtdb.firebaseio.com/products.json"
-          );
-          if (!response.ok) {
-            throw new Error("Something went wrong!");
-          }
-          const data = await response.json();
-          const loadedProducts = [];
+    // const [isLoading, setIsLoading] = useState(false);
+    // const [products, setProducts] = useState([]);
+    // const [error, setError] = useState(false);
+
+    // const fetchProductsHandler = useCallback(async () => {
+    //     setIsLoading(true);
+    //     setError(null);
+    //     try {
+    //       const response = await fetch(
+    //         "https://greenfood-3fadf-default-rtdb.firebaseio.com/products.json"
+    //       );
+    //       if (!response.ok) {
+    //         throw new Error("Something went wrong!");
+    //       }
+    //       const data = await response.json();
+    //       const loadedProducts = [];
     
-          for (const key in data) {
-            loadedProducts.push({
-              id: key,
-              name: data[key].name,
-              img: data[key].img,
-              desc: data[key].desc,
-              isEco: data[key].eco,
-              location: data[key].location,
-              voivodeship: data[key].voivodeship,
-              price: data[key].price,
-              unit: data[key].unit,
-              isFreeDelivery: data[key].isFreeDelivery,
-              freeDeliveryAmount: data[key].freeDeliveryAmount,
-            });
-          }
-          setProducts(loadedProducts);
-        } catch (error) {
-          setError(error.message);
-        }
-        setIsLoading(false);
-      }, []);
+    //       for (const key in data) {
+    //         loadedProducts.push({
+    //           id: key,
+    //           name: data[key].name,
+    //           location: data[key].location,
+    //           voivodeship: data[key].voivodeship,
+    //         });
+    //       }
+    //       setProducts(loadedProducts);
+    //     } catch (error) {
+    //       setError(error.message);
+    //     }
+    //     setIsLoading(false);
+    //   }, []);
     
-      useEffect(() => {
-        fetchProductsHandler();
-      }, [fetchProductsHandler]);
+    //   useEffect(() => {
+    //     fetchProductsHandler();
+    //   }, [fetchProductsHandler]);
 
-
+    //   const removeProductHandler = (id)=>{
+    //     const newProducts = products.filter(product => product.id !== id);
+    //     setProducts(newProducts);
+    //   }
 
     return (
         <>
             <h3>Edit/Remove Product</h3>
-
+            <ul className={classes['product-list']}>
+            {products.map((product) => {
+            return <ShortProduct key={product.id} productData={product} onRemoveProduct={onRemoveProduct}/>
+          })}
+            </ul>
         </>
 
     );
